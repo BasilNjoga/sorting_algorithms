81dda21fddaa78f3afc98b61da6fc30ed18f1ba2
Basil Njoga <Basilnjoga@gmail.com>
2022-07-18 12:44:48 +0300

    check

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - this is a function to sort out an array of integers 
+ * using bubble sort
+ * 
+ * @array: array of integers passed
+ * @size: size of integers requred
+ * 
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+int i;
+int j;
+size_t n = 10;
+int temp;
+
+for(j = 0; j < n; j++) 
+{
+    for (i = 0; i < n; i++)
+    {
+    if(array[i+1] < array[i])
+    {
+        temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+    }
+    print_array(array, n);
+    }
+}
+}
+
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
