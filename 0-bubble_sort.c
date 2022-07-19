4704340bc10885ac79639091087e389be18972ce
Basil Njoga <Basilnjoga@gmail.com>
2022-07-19 12:49:22 +0300

    check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,15 +17,15 @@ size_t i;
 size_t j;
 int temp;
 
-for(j = 0; j < size; j++)
+for (j = 0; j < size; j++)
 {
     for (i = 0; i < size; i++)
     {
-    if(array[i+1] < array[i])
+    if (array[i + 1] < array[i])
     {
         temp = array[i];
         array[i] = array[i+1];
-        array[i+1] = temp;
+        array[i + 1] = temp;
     }
     }
 }
