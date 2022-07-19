55dd4c658c525a4bc5ff4236031a89a8b1f6d27f
Basil <basilnjoga@gmail.com>
2022-07-19 15:10:37 -0700

    check

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,36 +4,34 @@
 
 /**
  * selection_sort - this is a function that perfoms a selection sort
- * 
+ *
  * @array: array to be sorted
  * @size: size of the array to be sorted
- * 
- * Return: Nothing 
+ * Return: Nothing
  */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t j;
-    size_t i;
-    int tempvalue;
-    int movingnumber;
-    int movingnumberindex;
+	size_t j;
+	size_t i;
+	int tempvalue;
+	int movingnumber;
+	int movingnumberindex;
 
-    for (i = 0; i < size - 1; i++)
-    {   
-        tempvalue = array[i];
-        movingnumber = array[i];
-        for (j = i; j < size - 1; j++)
-        {
-            if (tempvalue > array[j+1])
-                {
-                    tempvalue = array[j + 1];
-                    movingnumberindex = j + 1;
-                }
-        
-        }
-        array[i] = tempvalue;
-        array[movingnumberindex] = movingnumber;
-        print_array(array, size);
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		tempvalue = array[i];
+		movingnumber = array[i];
+		for (j = i; j < size - 1; j++)
+		{
+			if (tempvalue > array[j + 1])
+			{
+				tempvalue = array[j + 1];
+				movingnumberindex = j + 1;
+			}
+		}
+		array[i] = tempvalue;
+		array[movingnumberindex] = movingnumber;
+		print_array(array, size);
+	}
 }
