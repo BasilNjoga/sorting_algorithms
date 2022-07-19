a9d281b04f1dfbb9158a41c6cd4c4fb7a6f5bf99
Basil Njoga <Basilnjoga@gmail.com>
2022-07-20 00:22:36 +0300

    check

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a list using
+ * insertion sort
+ * 
+ * @list: list to be sorted 
+ * 
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    
+}
