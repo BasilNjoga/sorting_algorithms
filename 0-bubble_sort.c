31f0f062014eea5fe0d727d88e4aa5d581d8d80b
Basil <basilnjoga@gmail.com>
2022-07-19 02:01:20 -0700

    check

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,14 +14,13 @@
 
 void bubble_sort(int *array, size_t size)
 {
-int i;
-int j;
-size_t n = 10;
+size_t i;
+size_t j;
 int temp;
 
-for(j = 0; j < n; j++) 
+for(j = 0; j < size; j++) 
 {
-    for (i = 0; i < n; i++)
+    for (i = 0; i < size; i++)
     {
     if(array[i+1] < array[i])
     {
@@ -29,7 +28,7 @@ for(j = 0; j < n; j++)
         array[i] = array[i+1];
         array[i+1] = temp;
     }
-    print_array(array, n);
+    print_array(array, size);
     }
 }
 }
